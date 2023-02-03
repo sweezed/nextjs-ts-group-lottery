c634f547e05d0f2d6516b83714d85f266e666270
Edward Sweezey <edwardsweezey@Edwards-MacBook-Pro.local>
2023-02-02 21:56:42 -0500

    fixed work flow for lintNpretty.yml

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./**/tsconfig.json'],
-    tsconfigRootDir: __dirname,
-  },
+  plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    // will allow us to ! use in typescript. Just make sure we are guarantee it needs wil be assigned. dont even display warning
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/strict-boolean-expressions': 0,
+  },
+  env: {
+    node: true,
+  }
 };
