6e5dfb6f42cd4cb7c5db086f621e327726688bbc
Edward Sweezey <edwardsweezey@Edwards-MacBook-Pro.local>
2023-02-20 20:35:37 -0500

    add eslint rule back in for no console.logs

--- client/pages/groups/index.tsx
+++ client/pages/groups/index.tsx
@@ -1,7 +1,16 @@
-import { GroupScreens } from "../../components/wireframes/GroupsScreen"
+import React from 'react'
+import { GroupScreens } from '../../components/wireframes/GroupsScreen'
 
 function GroupsPage () {
-  <GroupScreens />
+  return (
+    <div className='page-container'>
+      <main className="flex-1 flex flex-col">
+        <div className="groups flex-1 flex">
+          <GroupScreens />
+        </div> 
+      </main> 
+    </div>
+  )
 }
 
 export default GroupsPage
