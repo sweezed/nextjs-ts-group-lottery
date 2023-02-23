064d000c7f03eb6aa2cf2919186489e3470f5bfa
sweezed <sweezed@users.noreply.github.com>
2023-02-23 21:01:29 +0000

    chore: lint & prettier

--- client/mobilePortFwd.js
+++ client/mobilePortFwd.js
@@ -7,10 +7,10 @@ const app = express()
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Cookie',
+    'Origin, X-Requested-With, Content-Type, Accept, Cookie'
   )
   next()
 })
@@ -31,22 +31,22 @@ app.use(
       // Log the response headers and status code
       log(`Response headers: ${JSON.stringify(proxyRes.headers)}`)
       log(`Response status code: ${proxyRes.statusCode}`)
-    }
+    },
   })
-);
+)
 
 const port = 2999
 
 app.listen(port, () => {
-   // Get the network interfaces for the current device
-   const networkInterfaces = os.networkInterfaces()
-   // Find the first network interface that is not internal and has an IPv4 address
-   const network = Object.values(networkInterfaces)
-     .flat()
-     .find((iface) => !iface.internal && iface.family === 'IPv4')
-   // Get the IPv4 address for the interface
-   const ipAddress = network.address
- 
-   log(`port forward set for mobile support on ${port}`)
-   log(`mobile url: http://${ipAddress}:${port}/`)
+  // Get the network interfaces for the current device
+  const networkInterfaces = os.networkInterfaces()
+  // Find the first network interface that is not internal and has an IPv4 address
+  const network = Object.values(networkInterfaces)
+    .flat()
+    .find((iface) => !iface.internal && iface.family === 'IPv4')
+  // Get the IPv4 address for the interface
+  const ipAddress = network.address
+
+  log(`port forward set for mobile support on ${port}`)
+  log(`mobile url: http://${ipAddress}:${port}/`)
 })
