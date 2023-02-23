2dada095b8d92ab5b94729a9ccbb763b5bc2f096
Edward Sweezey <edwardsweezey@Edwards-MacBook-Pro.local>
2023-02-23 16:08:07 -0500

    fixed auth test that was faiing

--- auth/src/__test__/signout.test.ts
+++ auth/src/__test__/signout.test.ts
@@ -21,7 +21,7 @@ describe('signout route', () => {
       .expect(200)
 
     expect(response2.get('set-cookie')[0]).toEqual(
-      'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;'
+      'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
     )
   })
 })
