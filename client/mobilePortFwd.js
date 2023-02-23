9f9d558db7fcc272c939ce3c5b661ef5a28a53c4
Edward Sweezey <edwardsweezey@Edwards-MacBook-Pro.local>
2023-02-23 15:58:29 -0500

    fix for mobile dev auth

--- client/mobilePortFwd.js
+++ client/mobilePortFwd.js
@@ -6,15 +6,12 @@ const { log } = require('@sweez/libs')
 const app = express()
 
 app.use((req, res, next) => {
-  // Allow cross-origin requests
   res.header('Access-Control-Allow-Origin', '*')
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Cookie',
   )
-
-  // uncomment to trouble shoot mobile requests
-  //log('**** mobile req.url:', req.url)
   next()
 })
 
@@ -25,21 +22,31 @@ app.use(
     target: 'https://grouplottery.com',
     changeOrigin: true,
     secure: false,
+    onProxyReq: (proxyReq, req, res) => {
+      if (req.headers) {
+        log('Proxy Req Headers:', req.headers)
+      }
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      // Log the response headers and status code
+      log(`Response headers: ${JSON.stringify(proxyRes.headers)}`)
+      log(`Response status code: ${proxyRes.statusCode}`)
+    }
   })
-)
+);
 
 const port = 2999
 
 app.listen(port, () => {
-  // Get the network interfaces for the current device
-  const networkInterfaces = os.networkInterfaces()
-  // Find the first network interface that is not internal and has an IPv4 address
-  const network = Object.values(networkInterfaces)
-    .flat()
-    .find((iface) => !iface.internal && iface.family === 'IPv4')
-  // Get the IPv4 address for the interface
-  const ipAddress = network.address
-
-  log(`port forward set for mobile support on ${port}`)
-  log(`mobile url: ${ipAddress}:${port}/`)
+   // Get the network interfaces for the current device
+   const networkInterfaces = os.networkInterfaces()
+   // Find the first network interface that is not internal and has an IPv4 address
+   const network = Object.values(networkInterfaces)
+     .flat()
+     .find((iface) => !iface.internal && iface.family === 'IPv4')
+   // Get the IPv4 address for the interface
+   const ipAddress = network.address
+ 
+   log(`port forward set for mobile support on ${port}`)
+   log(`mobile url: http://${ipAddress}:${port}/`)
 })
