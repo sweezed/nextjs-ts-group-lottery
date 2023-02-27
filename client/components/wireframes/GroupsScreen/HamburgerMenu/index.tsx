053d96088cf6afe193b55c7b907014641c0fba3d
sweezed <sweezed@users.noreply.github.com>
2023-02-27 18:49:36 +0000

    chore: lint & prettier

--- client/components/wireframes/GroupsScreen/HamburgerMenu/index.tsx
+++ client/components/wireframes/GroupsScreen/HamburgerMenu/index.tsx
@@ -4,61 +4,63 @@ import { LotteryBallIcon } from '../LotteryBall'
 interface IHamburgerProps {
   show: boolean
 }
-const HambugerIcon = ({show, openMenu}) => {
+const HambugerIcon = ({ show, openMenu }) => {
   if (!show) {
     return null
   }
 
   return (
     <div className="flex justify-end">
-    <button
-      className="m-2 items-center border-2 px-3 py-3
+      <button
+        className="m-2 items-center border-2 px-3 py-3
       rounded-md shadow-[0_0px_5px_7px_rgba(0,0,0,0.2)]
      text-secondary-accent bg-white border-secondary-accent hover:bg-gray-300 active:bg-gray-400"
-      onClick={openMenu}
-    >
-      <svg className=" w-4 h-4 border-1 border-green-300">
-        <rect
-          x="0"
-          y="0"
-          width="20"
-          height="2"
-          rx="1"
-          fill="currentColor"
-        />
-        <rect
-          x="0"
-          y="7"
-          width="20"
-          height="2"
-          rx="1"
-          fill="currentColor"
-        />
-        <rect
-          x="0"
-          y="14"
-          width="20"
-          height="2"
-          rx="1"
-          fill="currentColor"
-        />
-      </svg>
-    </button>
-  </div>
+        onClick={openMenu}
+      >
+        <svg className=" w-4 h-4 border-1 border-green-300">
+          <rect
+            x="0"
+            y="0"
+            width="20"
+            height="2"
+            rx="1"
+            fill="currentColor"
+          />
+          <rect
+            x="0"
+            y="7"
+            width="20"
+            height="2"
+            rx="1"
+            fill="currentColor"
+          />
+          <rect
+            x="0"
+            y="14"
+            width="20"
+            height="2"
+            rx="1"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
   )
 }
-const CloseIcon = ({onClose}) => {
+const CloseIcon = ({ onClose }) => {
   return (
-    <div 
+    <div
       className="w-8 h-8 rounded-full shadow-[0_0px_2px_3px_rgba(0,0,0,0.3)]
         bg-secondary-accent 
         text-white flex items-center justify-center
         hover:bg-primary"
       onClick={onClose}
-    >x</div>
+    >
+      x
+    </div>
   )
 }
-const MenuDrawer = ({show, closeMenu}) => {
+const MenuDrawer = ({ show, closeMenu }) => {
   if (!show) {
     return null
   }
@@ -75,30 +77,30 @@ const MenuDrawer = ({show, closeMenu}) => {
           font-bold
           text-lg
           p-4"
-      > 
+      >
         <div className="flex-1 border-2 border-primary rounded-2xl flex-col relative">
-          <div className='absolute top-0 left-0 transform -translate-x-1/4 -translate-y-1/4'>
+          <div className="absolute top-0 left-0 transform -translate-x-1/4 -translate-y-1/4">
             <CloseIcon onClose={closeMenu} />
           </div>
-          <ul className='m-8'>
-            <li className='p-2 flex '>
-              <LotteryBallIcon number='1' />
+          <ul className="m-8">
+            <li className="p-2 flex ">
+              <LotteryBallIcon number="1" />
               <div className="ml-2">Moderator</div>
             </li>
-            <li className='p-2 flex '>
-              <LotteryBallIcon number='2' />
+            <li className="p-2 flex ">
+              <LotteryBallIcon number="2" />
               <div className="ml-2">Show Only Your Tickets</div>
             </li>
-            <li className='p-2 flex '>
-              <LotteryBallIcon number='3' />
+            <li className="p-2 flex ">
+              <LotteryBallIcon number="3" />
               <div className="ml-2">Show All Tickets</div>
             </li>
-            <li className='p-2 flex '>
-              <LotteryBallIcon number='4' />
+            <li className="p-2 flex ">
+              <LotteryBallIcon number="4" />
               <div className="ml-2">Review Rules & Regulations</div>
             </li>
-            <li className='p-2 flex '>
-              <LotteryBallIcon number='5' />
+            <li className="p-2 flex ">
+              <LotteryBallIcon number="5" />
               <div className="ml-2">Log Off</div>
             </li>
           </ul>
@@ -122,8 +124,14 @@ const HamburgerMenu: React.FC<IHamburgerProps> = ({ show }) => {
 
   return (
     <div className="sm:hidden z-10 absolute top-0 right-0 w-screen">
-      <HambugerIcon show={!expanded} openMenu={handleOpen} />
-      <MenuDrawer show={expanded} closeMenu={handleClose} />
+      <HambugerIcon
+        show={!expanded}
+        openMenu={handleOpen}
+      />
+      <MenuDrawer
+        show={expanded}
+        closeMenu={handleClose}
+      />
     </div>
   )
 }
--- client/components/wireframes/GroupsScreen/LotteryBall/index.tsx
+++ client/components/wireframes/GroupsScreen/LotteryBall/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from 'react'
 
 function LotteryBallIcon({ number }) {
   return (
-  <div className="w-12 h-12 perspective-400">
-    <div className="w-12 h-12 relative transform-style-preserve-3d rotate-x-20 rotate-y-20">
-      <div className="w-12 h-12 absolute border border-gray-400 rounded-full bg-white shadow-md flex justify-center items-center text-lg font-bold text-black transform-style-preserve-3d">
-        <span className="transform translate-z-5">{number}</span>
+    <div className="w-12 h-12 perspective-400">
+      <div className="w-12 h-12 relative transform-style-preserve-3d rotate-x-20 rotate-y-20">
+        <div className="w-12 h-12 absolute border border-gray-400 rounded-full bg-white shadow-md flex justify-center items-center text-lg font-bold text-black transform-style-preserve-3d">
+          <span className="transform translate-z-5">{number}</span>
+        </div>
+        <div
+          className="w-12 h-12 absolute border border-gray-400 rounded-full bg-white shadow-md transform translate-z-neg-12 rotate-y-90"
+          style={{ transformStyle: 'preserve-3d', transform: 'rotateY(90deg)' }}
+        ></div>
       </div>
-      <div className="w-12 h-12 absolute border border-gray-400 rounded-full bg-white shadow-md transform translate-z-neg-12 rotate-y-90" style={{ transformStyle: 'preserve-3d', transform: 'rotateY(90deg)' }}></div>
     </div>
-  </div>
   )
 }
 
-export {
-  LotteryBallIcon
-} 
+export { LotteryBallIcon }
